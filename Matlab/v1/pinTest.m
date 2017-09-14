221241163677c9485a67ef9a7ad7b63090efb729
NuwanJ <nuwanjaliyagoda@gmail.com>
2017-09-14 22:17:31 +0530

    14/09/2017 #2

--- Matlab/v1/pinTest.m
+++
@@ -1,7 +0,0 @@
-
-
-while 1==1
-   b = analogRead(a, 0);
-   disp(b );
-   pause(0.5);
-end
---
+++ Matlab/v3/readPins.m
@@ -0,0 +1,55 @@
+
+function inPins = readPins()
+
+    analogRange = linspace(0,1023,7);
+    inPins =0;
+
+    for readBtn = 1:3
+        reading = analogRead(a, readBtn);
+        
+        if (reading>=analogRange(1) && reading<analogRange(2))
+            inPins = 3*(readBtn-1) + 1;
+        elseif (reading>=analogRange(2) && reading<analogRange(4))
+            inPins = 3*(readBtn-1) + 2;
+        elseif (reading>=analogRange(4) && reading<analogRange(6))
+            inPins = 3*(readBtn-1) + 3;
+        else
+            % Nothing yet
+        end
+    end
+    
+    % Return corresponding button value 1 to 9
+return
+
+
+
+%{
+
+analogRange = linspace(0,1023,7);
+oldReading  = 0;
+
+while 1==1
+   b = analogRead(a, 0);
+   s = 0;
+
+   if (b>=analogRange(1) && b<analogRange(2))
+       s=1;
+   elseif (b>=analogRange(2) && b<analogRange(4))
+       s=2;
+   elseif (b>=analogRange(4) && b<analogRange(6))
+       s=3; 
+   else
+       s=0; 
+   end
+   
+   if (b~= oldReading)
+      fprintf('%d --> %d \n', b, s)
+      oldReading = b;
+   else 
+        pause(.001);
+   end
+end
+
+
+
+%}
